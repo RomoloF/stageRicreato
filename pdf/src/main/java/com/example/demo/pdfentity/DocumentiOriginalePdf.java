07523e9965cbdc6b56497ff6db70fd6b65887a8e
Romolo1964 <fiorenzaromolo4@gmail.com>
2024-07-24 12:05:40 +0200

    Aggiunto numero 3 entity class

--- pdf/src/main/java/com/example/demo/pdfentity/DocumentiOriginalePdf.java
+++ pdf/src/main/java/com/example/demo/pdfentity/DocumentiOriginalePdf.java
@@ -1,8 +1,16 @@
-package pdfentity;
+package com.example.demo.pdfentity;
 
 import java.io.Serializable;
-import javax.persistence.*;
 import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.NamedQuery;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 
 /**
